ba1b41c2fa4613aaf4d5e90dd01a9ed44a177408
Alexandre Sousa <eoloc57@gmail.com>
2021-11-24 23:57:11 +0100

    Update Jeu.java - win and stop game

--- src/main/java/main/tp_puissance4/Jeu.java
+++ src/main/java/main/tp_puissance4/Jeu.java
@@ -1,12 +1,17 @@
 package main.tp_puissance4;
 
+import main.tp_puissance4.Grille;
+import main.tp_puissance4.Joueur;
+
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.util.Scanner;
 
 public class Jeu {
     private boolean fin_jeu;
     private Grille grille;
+
 
     public Jeu(Grille g) {
         fin_jeu = false;
@@ -17,41 +22,19 @@ public class Jeu {
         Joueur joueur1 = new Joueur(1, 'O');
         Joueur joueur2 = new Joueur(2, 'X');
         int tour = 1;
-        char commande = '-';
-        Reader reader = new InputStreamReader(System.in);
         boolean bonChoix = true;
+        Scanner sc = new Scanner(System.in);
+        int choix;
 
         while(!fin_jeu){
             if(tour == joueur1.getIdTour()){
                 System.out.println("Joueur 1 jouer");
 
                 bonChoix = true;
-
-                commande = (char) reader.read();
-                reader.skip(1);
-
-                if(commande == '1'){
-                    insererJeton(joueur1, 0);
-                }
-                else if(commande == '2'){
-                    insererJeton(joueur1, 1);
-                }
-                else if(commande == '3'){
-                    insererJeton(joueur1, 2);
-                }
-                else if(commande == '4'){
-                    insererJeton(joueur1, 3);
-                }
-                else if(commande == '5'){
-                    insererJeton(joueur1, 4);
-                }
-                else if(commande == '6'){
-                    insererJeton(joueur1, 5);
-                }
-                else if(commande == '7'){
-                    insererJeton(joueur1, 6);
-                }
-                else {
+                try {
+                    choix = sc.nextInt();
+                    insererJeton(joueur1, choix-1);
+                } catch(Exception e) {
                     bonChoix = false;
                 }
             }
@@ -60,33 +43,13 @@ public class Jeu {
 
                 bonChoix = true;
 
-                commande = (char) reader.read();
-                reader.skip(1);
-
-                if(commande == '1'){
-                    insererJeton(joueur2, 0);
-                }
-                else if(commande == '2'){
-                    insererJeton(joueur2, 1);
-                }
-                else if(commande == '3'){
-                    insererJeton(joueur2, 2);
-                }
-                else if(commande == '4'){
-                    insererJeton(joueur2, 3);
-                }
-                else if(commande == '5'){
-                    insererJeton(joueur2, 4);
-                }
-                else if(commande == '6'){
-                    insererJeton(joueur2, 5);
-                }
-                else if(commande == '7'){
-                    insererJeton(joueur2, 6);
-                }
-                else {
+                try {
+                    choix = sc.nextInt();
+                    insererJeton(joueur2, choix-1);
+                } catch(Exception e) {
                     bonChoix = false;
                 }
+
             }
             if(bonChoix){
                 if(tour == 1) {
@@ -95,21 +58,115 @@ public class Jeu {
                     tour = 1;
                 }
             }
-            commande = '-';
             System.out.println(grille);
         }
+        System.out.println("Jeu terminé");
     }
 
     public void insererJeton(Joueur joueur, int position){
         for(int j = 0; j < grille.getTailleY(); j++){
             if(grille.getGrilleChar(position, j) != '-'){
                 grille.setGrilleChar(position, j-1, joueur.getJeton());
+                fin_jeu = verifWin(position, j-1);
+                if(fin_jeu){
+                    System.out.println("Bravo a joueur" + joueur.getIdTour());
+                }
                 break;
             }
             if(j == grille.getTailleY() - 1){
                 grille.setGrilleChar(position, j, joueur.getJeton());
+                fin_jeu = verifWin(position, j);
+                if(fin_jeu){
+                    System.out.println("Bravo a joueur" + joueur.getIdTour());
+                }
             }
 
         }
+    }
+
+    public boolean verifWin(int x, int y) {
+        char jetonActuel = grille.getGrilleChar(x, y);
+        boolean win = false;
+        boolean gauche = true;
+        boolean droite = true;
+        boolean haut = true;
+        boolean bas = true;
+        boolean gauchehaut = true;
+        boolean droitehaut = true;
+        boolean gauchebas = true;
+        boolean droitebas = true;
+        for(int i = 1; i < 4; i++) {
+            /*System.out.println("tour de boucle " + i + " x:" + x + " y:" + y);
+            System.out.println("pour gauche : x:" + (x-i) + " y:" + y);
+            System.out.println("pour droite : x:" + (x+i) + " y:" + y);
+            System.out.println("pour haut : x:" + x + " y:" + (y-1));
+            System.out.println("pour bas : x:" + x + " y:" + (y+1));
+            System.out.println("pour gauchehaut : x:" + (x-1) + " y:" + (y-1));
+            System.out.println("pour droitehaut : x:" + (x+1) + " y:" + (y-1));
+            System.out.println("pour gauchebas : x:" + (x-1) + " y:" + (y+1));
+            System.out.println("pour droitebas : x:" + (x+1) + " y:" + (y+1) +"\n\n");*/
+            try {
+                if(grille.getGrilleChar(x-i, y) != jetonActuel){
+
+                    gauche = false;
+                }
+            } catch (Exception e){
+                gauche = false;
+            }
+            try {
+                if(grille.getGrilleChar(x+i, y) != jetonActuel){
+                    droite = false;
+                }
+            } catch (Exception e){
+                droite = false;
+            }
+            try {
+                if(grille.getGrilleChar(x, y-i) != jetonActuel){
+                    haut = false;
+                }
+            } catch(Exception e) {
+                haut = false;
+            }
+            try {
+                if(grille.getGrilleChar(x, y+i) != jetonActuel){
+                    bas = false;
+                }
+            } catch(Exception e) {
+                bas = false;
+            }
+            try {
+                if(grille.getGrilleChar(x-i, y-i) != jetonActuel){
+                    gauchehaut = false;
+                }
+            } catch(Exception e) {
+                gauchehaut = false;
+            }
+            try {
+                if(grille.getGrilleChar(x+i, y-i) != jetonActuel){
+                    droitehaut = false;
+                }
+            } catch(Exception e) {
+                droitehaut = false;
+            }
+            try {
+                if(grille.getGrilleChar(x-i, y+i) != jetonActuel){
+                    gauchebas = false;
+                }
+            } catch(Exception e) {
+                gauchebas = false;
+            }
+            try {
+                if(grille.getGrilleChar(x+i, y+i) != jetonActuel){
+                    droitebas = false;
+                }
+            } catch(Exception e) {
+                droitebas = false;
+            }
+        }
+        //System.out.println("gauche : " + gauche + "\n" + "droite : " + droite + "\n" + "haut : " + haut + "\n" + "bas : " + bas + "\n" + "gauchehaut : " + gauchehaut + "\n" + "droitehaut : " + droitehaut + "\n" + "gauchebas : " + gauchebas + "\n" + "droitebas : " + droitebas + "\n");
+        if(gauche || droite || haut || bas || gauchehaut || droitehaut || gauchebas || droitebas){
+            win = true;
+        }
+        return win;
     }
 }
